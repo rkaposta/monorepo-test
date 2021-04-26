60878cfff1a7c8c8cfa185e7a4dcd407eb428420
Robert Kaposta <rkaposta@gmail.com>
2021-04-26 14:57:59 +0200

    chore: add dep

--- packages/foo-pkg/lib/foo-pkg.js
+++ packages/foo-pkg/lib/foo-pkg.js
@@ -1,8 +1,13 @@
 'use strict';
 
+const barPkg = require("@rkaposta-test/bar-pkg");
+
 module.exports = fooPkg;
 
 function fooPkg() {
     // TODO
 	console.log('Hello world!!!!!!!?<!');
+    barPkg();
 }
+
+fooPkg();
