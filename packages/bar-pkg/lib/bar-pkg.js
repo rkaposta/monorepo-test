d38e52f91a16825dc43d8d1d66cf4591152c3cb7
Robert Kaposta <rkaposta@gmail.com>
2021-04-26 15:51:26 +0200

    feat: extended text\nBREAKING CHANGE: this is some good stuff

--- packages/bar-pkg/lib/bar-pkg.js
+++ packages/bar-pkg/lib/bar-pkg.js
@@ -3,5 +3,5 @@
 module.exports = barPkg;
 
 function barPkg() {
-    console.log('// TODO - bar pkg new version, yihaaaa4 //');
+    console.log('// TODO - bar pkg new version, yihaaaa4 - MAJOR //');
 }
